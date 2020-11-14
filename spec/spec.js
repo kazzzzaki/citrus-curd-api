d511925b3462597661153bd7af4b5fe4cec7a3dc
kazzzzzzaki <kazuakiu0327@gmail.com>
2020-11-14 15:16:40 +0900

    created todo in spec

--- spec/spec.js
+++ spec/spec.js
@@ -30,105 +30,158 @@ describe("tasklist API server", () => {
   });
 
   describe("tasklist API test", () => {
-    describe("GET /api/user - get users data", () => {
-      //TODO:データまで確認する方法が見つかったら、修正した
-      it("should return all users", async () => {
-        //SETUP
-        const userData = await db.user.findAll({ raw: true });
-        const expect = userData.map((user) => {
-          user.createdAt = user.createdAt.toJSON();
-          user.updatedAt = user.updatedAt.toJSON();
-          return user;
+    describe("/api/user test", () => {
+      describe("GET /api/user - get users data", () => {
+        //TODO:データまで確認する方法が見つかったら、修正した
+        it("should return all users", async () => {
+          //SETUP
+          const userData = await db.user.findAll({ raw: true });
+          const expect = userData.map((user) => {
+            user.createdAt = user.createdAt.toJSON();
+            user.updatedAt = user.updatedAt.toJSON();
+            return user;
+          });
+
+          //EXCERCISE
+          const res = await request.get("/api/user");
+
+          //ASSERT
+          res.should.have.status(200);
+          JSON.parse(res.text).should.deep.equal(expect);
+
+          //TEARDOWN
         });
 
-        //EXCERCISE
-        const res = await request.get("/api/user");
+        it("should return users limit 3", async () => {
+          //SETUP
+          const userData = await db.user.findAll({ raw: true, limit: 3 });
+          const expect = userData.map((user) => {
+            user.createdAt = user.createdAt.toJSON();
+            user.updatedAt = user.updatedAt.toJSON();
+            return user;
+          });
+          //EXCERCISE
+          const res = await request.get("/api/user?limit=3");
 
-        //ASSERT
-        res.should.have.status(200);
-        JSON.parse(res.text).should.deep.equal(expect);
+          //ASSERT
+          res.should.have.status(200);
+          JSON.parse(res.text).should.deep.equal(expect);
 
-        //TEARDOWN
+          //TEARDOWN
+        });
+
+        it("should return users limit 2 offset 2", async () => {
+          //SETUP
+          const userData = await db.user.findAll({
+            raw: true,
+            limit: 2,
+            offset: 2,
+          });
+          const expect = userData.map((user) => {
+            user.createdAt = user.createdAt.toJSON();
+            user.updatedAt = user.updatedAt.toJSON();
+            return user;
+          });
+          //EXCERCISE
+          const res = await request.get("/api/user?limit=2&offset=2");
+
+          //ASSERT
+          res.should.have.status(200);
+          JSON.parse(res.text).should.deep.equal(expect);
+
+          //TEARDOWN
+        });
+        it("should return users by id", async () => {
+          //SETUP
+          const userData = await db.user.findAll({
+            raw: true,
+            where: { id: 1 },
+          });
+          const expect = userData.map((user) => {
+            user.createdAt = user.createdAt.toJSON();
+            user.updatedAt = user.updatedAt.toJSON();
+            return user;
+          });
+          //EXCERCISE
+          const res = await request.get("/api/user/1");
+
+          //ASSERT
+          res.should.have.status(200);
+          JSON.parse(res.text).should.deep.equal(expect);
+
+          //TEARDOWN
+        });
+        it("should return users by name", async () => {
+          //SETUP
+          const userData = await db.user.findAll({
+            raw: true,
+            where: { name: "kazuaki" },
+          });
+          const expect = userData.map((user) => {
+            user.createdAt = user.createdAt.toJSON();
+            user.updatedAt = user.updatedAt.toJSON();
+            return user;
+          });
+          //EXCERCISE
+          const res = await request.get("/api/user/kazuaki");
+
+          //ASSERT
+          res.should.have.status(200);
+          JSON.parse(res.text).should.deep.equal(expect);
+
+          //TEARDOWN
+        });
+
+        describe("POST /api/user - get users data", () => {
+          //TODO:データまで確認する方法が見つかったら、修正した
+          it("should return all users", async () => {
+            //SETUP
+            const userData = await db.user.findAll({ raw: true });
+            const expect = userData.map((user) => {
+              user.createdAt = user.createdAt.toJSON();
+              user.updatedAt = user.updatedAt.toJSON();
+              return user;
+            });
+
+            //EXCERCISE
+            const res = await request.get("/api/user");
+
+            //ASSERT
+            res.should.have.status(200);
+            JSON.parse(res.text).should.deep.equal(expect);
+
+            //TEARDOWN
+          });
+        });
+        describe("POST /api/user - get users data", () => {
+          //TODO:
+        });
+        describe("PATCH /api/user - get users data", () => {
+          //TODO:
+        });
+        describe("PUT /api/user - get users data", () => {
+          //TODO:
+        });
+        describe("DELETE /api/user - get users data", () => {
+          //TODO:
+        });
       });
-
-      it("should return users limit 3", async () => {
-        //SETUP
-        const userData = await db.user.findAll({ raw: true, limit: 3 });
-        const expect = userData.map((user) => {
-          user.createdAt = user.createdAt.toJSON();
-          user.updatedAt = user.updatedAt.toJSON();
-          return user;
-        });
-        //EXCERCISE
-        const res = await request.get("/api/user?limit=3");
-
-        //ASSERT
-        res.should.have.status(200);
-        JSON.parse(res.text).should.deep.equal(expect);
-
-        //TEARDOWN
+    });
+    describe("/api/task test", () => {
+      describe("GET /api/task - get task data", () => {
+        //TODO: task
       });
-
-      it("should return users limit 2 offset 2", async () => {
-        //SETUP
-        const userData = await db.user.findAll({
-          raw: true,
-          limit: 2,
-          offset: 2,
-        });
-        const expect = userData.map((user) => {
-          user.createdAt = user.createdAt.toJSON();
-          user.updatedAt = user.updatedAt.toJSON();
-          return user;
-        });
-        //EXCERCISE
-        const res = await request.get("/api/user?limit=2&offset=2");
-
-        //ASSERT
-        res.should.have.status(200);
-        JSON.parse(res.text).should.deep.equal(expect);
-
-        //TEARDOWN
+      describe("POST /api/task - get task data", () => {
+        //TODO: task
       });
-      it("should return users by id", async () => {
-        //SETUP
-        const userData = await db.user.findAll({
-          raw: true,
-          where: { id: 1 },
-        });
-        const expect = userData.map((user) => {
-          user.createdAt = user.createdAt.toJSON();
-          user.updatedAt = user.updatedAt.toJSON();
-          return user;
-        });
-        //EXCERCISE
-        const res = await request.get("/api/user/1");
-
-        //ASSERT
-        res.should.have.status(200);
-        JSON.parse(res.text).should.deep.equal(expect);
-
-        //TEARDOWN
+      describe("PATCH /api/task - get task data", () => {
+        //TODO: task
       });
-      it("should return users by name", async () => {
-        //SETUP
-        const userData = await db.user.findAll({
-          raw: true,
-          where: { name: "kazuaki" },
-        });
-        const expect = userData.map((user) => {
-          user.createdAt = user.createdAt.toJSON();
-          user.updatedAt = user.updatedAt.toJSON();
-          return user;
-        });
-        //EXCERCISE
-        const res = await request.get("/api/user/kazuaki");
-
-        //ASSERT
-        res.should.have.status(200);
-        JSON.parse(res.text).should.deep.equal(expect);
-
-        //TEARDOWN
+      describe("PUT /api/task - get task data", () => {
+        //TODO: task
+      });
+      describe("DELETE /api/task - get task data", () => {
+        //TODO: task
       });
     });
   });
